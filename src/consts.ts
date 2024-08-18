8fa346a8dc7d22d3b91c91aa5991583da71ecf1e
joshuamotoaki <dachshunduniverse@gmail.com>
2024-08-17 20:14:13 -0600

    Change name

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Joshua Motoaki Lau';
+export const SITE_DESCRIPTION = 'Joshua Motoaki Lau\'s personal website and blog.';
